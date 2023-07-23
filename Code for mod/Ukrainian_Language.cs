ed58034e4351d0b527836ab65f5173853464f675
Damglador <vse.stopchanskyi@gmail.com>
2023-07-23 20:06:30 +0300

    шось

--- Code for mod/Ukrainian_Language.cs
+++ Code for mod/Ukrainian_Language.cs
@@ -3,8 +3,10 @@ using BerryLoaderNS;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using TMPro;
 using System.Reflection;
 using UnityEngine;
+using HarmonyLib;
 
 namespace Ukrainian_Language
 {
@@ -13,10 +15,16 @@ namespace Ukrainian_Language
     class Plugin : BaseUnityPlugin
     {
         public static BepInEx.Logging.ManualLogSource L;
-
+        public static AssetBundle FontBundle;
+        public static string BundlePath;
         private void Awake()
         {
             L = Logger;
+            BundlePath = Path.Combine(Directory.GetParent(this.Info.Location).ToString(), "mod.bundle");
+            Plugin.L.LogInfo(Directory.GetParent(this.Info.Location).ToString());
+            var harmony = new Harmony("Plugin");
+            harmony.PatchAll(typeof(Patches));
+
 
             var translationFilePath = Path.Combine(Directory.GetParent(this.Info.Location).FullName, "translation.tsv");
 
@@ -37,6 +45,26 @@ namespace Ukrainian_Language
             Dictionary<string, string> langNames = (Dictionary<string, string>)langNamesField.GetValue(null);
             langNames.Add("Ukrainian", "Українська");
             langNamesField.SetValue(null, langNames);
+        }
+    }
+    class Patches
+    {
+        [HarmonyPatch(typeof(FontManager), "Awake")]
+        [HarmonyPostfix]
+        public static void FMAwake()
+        {
+            Plugin.L.LogInfo("Loading fixed font...");
+            if (Plugin.FontBundle == null)
+                Plugin.FontBundle = AssetBundle.LoadFromFile(Plugin.BundlePath);
+
+            if (Plugin.FontBundle == null)
+            {
+                Plugin.L.LogError("Failed to load Font AssetBundle!");
+                return;
+            }
+
+            FontManager.instance.WorldFontAsset = Plugin.FontBundle.LoadAsset<TMP_FontAsset>("NotoSans-Bold SDF");
+            FontManager.instance.RegularFontAsset = Plugin.FontBundle.LoadAsset<TMP_FontAsset>("NotoSans-Bold SDF");
         }
     }
 }
---
+++ Translation/Mod.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TMPro;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Ukrainian_Language
+{
+    public class Ukrainian_Language : Mod
+    {
+        public static AssetBundle FontBundle;
+        public override void Ready()
+        {
+            var harmony = new Harmony("Ukrainian_Language");
+            harmony.PatchAll(typeof(Patches));
+
+            Logger.Log("Ready!");
+            var langs = SokLoc.Languages.ToList<SokLanguage>();
+            langs.Add(new SokLanguage { LanguageName = "Ukrainian", UnitySystemLanguage = SystemLanguage.Ukrainian });
+            SokLoc.Languages = langs.ToArray();
+
+            var langNamesField = typeof(SokLoc).GetField("localLanguageNames", BindingFlags.NonPublic | BindingFlags.Static);
+            Dictionary<string, string> langNames = (Dictionary<string, string>)langNamesField.GetValue(null);
+            langNames.Add("Ukrainian", "Українська");
+            langNamesField.SetValue(null, langNames);
+        }
+    }
+}
